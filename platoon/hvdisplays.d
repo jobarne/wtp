fa4479b41c229e2d814d6494e96bec99a121a3a8
jobarne <johan.barne@gmail.com>
2017-10-19 15:57:27 +0200

    Create hvdisplays.d
    Introduced 2015-05-07

---
+++ platoon/hvdisplays.d
@@ -0,0 +1,18 @@
+#############
+# name Device ID //Prestart   Upwind   Reaching   Downwind
+#############
+
+[10/10]
+
+[20/20]
+mast1   1   91   91   91   91
+mast2   2   -1   32   32   32
+mast3   3   16   16   16   16
+mast4   4   34   34   34   34
+mast5   5   18   18   18   18
+mast6   6   89   89   89   89
+mast7   7   17   17   17   17
+
+[30/30]
+
+[40/40]

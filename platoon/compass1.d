0ad8e901ea88b68b8b7e8abfb6d2e6bbd010e31c
jobarne <johan.barne@gmail.com>
2017-10-19 15:19:34 +0200

    Update compass1.d

--- platoon/compass1.d
+++ platoon/compass1.d
@@ -1,7 +1,7 @@
 Channel 0
 Device 2
 
-Com 1 RS232 28800 N 8 1 BGIMU 
+Com 1 RS232 38400 N 8 1 BGIMU 
 
 heading1  1  13  heading1.cal  heading1.fil 	null.alm
 heel1     1   0  heel1.cal     heel1.fil		null.alm

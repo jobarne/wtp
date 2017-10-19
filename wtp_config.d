c50ebfceae0f549cb97dc0a075a0d2d2b39f42e2
jobarne <johan.barne@gmail.com>
2017-10-19 11:23:32 +0200

    Add files via upload

---
+++ wtp_config.d
@@ -0,0 +1,23 @@
+[config]
+
+Dir platoon
+
+
+[Diagnostics]
+#can 1 3
+#can channel 0, device 0
+#fastnet_stats on
+#fastnet messages, on will show, off will not show
+#fastnet_rx on
+#displays incoming fastnet messages into the wtp3, halcyon 2000 compass for example
+
+fastnet_tx on
+
+#displays the fastnet messages going out to the network
+
+
+fastnet_stats on
+#can 0 1234
+#can 1 456
+
+
